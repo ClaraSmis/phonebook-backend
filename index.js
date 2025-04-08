b71ce5b6f4ab4d2b97c54384a5bd79a04ffac08c
ClaraSmis <clara.smis@outlook.fr>
2025-04-08 16:22:35 +0200

    uibuild

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const app = express()
 const morgan  = require('morgan')
-const cors = require('cors')
 
-app.use(cors())
 app.use(express.json())
 morgan.token('request-body', (request, response) =>
     {
